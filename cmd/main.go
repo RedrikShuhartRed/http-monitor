349336698a01e880f6e72fc2e054bddc383b1b81
Oleg Ilyin <ilynrabota@yandex.ru>
2024-05-17 20:56:42 +0500

    almost done

--- cmd/main.go
+++ cmd/main.go
@@ -2,46 +2,30 @@ package main
 
 import (
 	"fmt"
-	"net/http"
 	"os"
-	"time"
+	"sync"
 
+	"github.com/RedrikShuhartRed/http-monitor/handlers"
 	_ "github.com/go-sql-driver/mysql"
 )
 
-type RequestInfo struct {
-	URL           string
-	TimeRequest   time.Time
-	TimeResponse  time.Time
-	CodeResponse  int
-	ContentLength string
-}
-
 func main() {
+	var wg sync.WaitGroup
 
-	// db, err := sql.Open("mysql", "root:root@tcp(127.0.0.1:3306)/http-monitor")
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// db.Query("CREATE TABLE monitor(
-	// 	url
-	// )")
+	link := os.Args
+	if len(link) < 2 {
+		fmt.Println("Enter link example: http://jojo.ru")
+		return
+	}
+	url := link[1]
 
-	RequestInfo := RequestInfo{}
-	RequestInfo.URL = os.Args[1]
-	fmt.Println(len(RequestInfo.URL))
-	fmt.Println(RequestInfo.URL)
-	RequestInfo.TimeRequest = time.Now()
-	resp, _ := http.Get(RequestInfo.URL)
-	RequestInfo.ContentLength = resp.Header.Get("Content-Length")
-	RequestInfo.CodeResponse = resp.StatusCode
-	defer resp.Body.Close()
-	fmt.Println(resp.StatusCode)
-	RequestInfo.TimeResponse = time.Now()
-	reqDuration := RequestInfo.TimeResponse.Sub(RequestInfo.TimeRequest)
-	fmt.Println(RequestInfo.TimeResponse.Format("2006-01-02 15:04:05"))
-	fmt.Println(RequestInfo.TimeRequest.Format("2006-01-02 15:04:05"))
-	fmt.Println(reqDuration)
-	fmt.Println(RequestInfo)
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go func() {
+			handlers.Get(url)
+			defer wg.Done()
+		}()
 
+	}
+	wg.Wait()
 }
